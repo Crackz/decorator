ce38aab2ab3a740c044f675542a1af7fb6285782
Nader-mech <vipa00magdy00@gmail.com>
2020-01-03 16:01:37 +0200

    Restructure AppAlet

--- src/components/Shared/AppAlert.jsx
+++ src/components/Shared/AppAlert.jsx
@@ -1,13 +1,18 @@
-import { UncontrolledAlert } from "reactstrap";
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert } from "reactstrap";
 
 
 const AppAlert = (props) => {
+    const [visible, setVisible] = useState(true);
+    const onDismiss = () => setVisible(false);
+
+
     return (
         <>
-            <UncontrolledAlert style={{ marginTop: "20px", textAlign: 'center' }} color="danger">
-                {props.message}
-            </UncontrolledAlert>
+            <Alert color="danger" style={{ marginTop: "20px", textAlign: 'center' }} fade={true} isOpen={visible}>
+                <i style={{ position: 'absolute', top: 5, right: 5, cursor: 'pointer' }} className="fas fa-window-close" onClick={() => onDismiss()}></i>
+                <span>{props.message}</span>
+            </Alert>
         </>
     )
 }
